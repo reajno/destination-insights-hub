9e363469fc0477696d5dd03fafc13bbbc92a2909
ajr <reanojohn@gmail.com>
2025-06-28 13:51:08 +1000

    Update import path

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "@/components/NavBar";
+import NavBar from "@/components/nav/NavBar";
 import { Toaster } from "@/components/chakra-ui/toaster";
 
 const MainLayout = () => {
