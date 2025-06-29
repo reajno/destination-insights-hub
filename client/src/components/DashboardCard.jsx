658a53203f66e686e6601206edfd194b16383f87
ajr <reanojohn@gmail.com>
2025-06-29 18:53:10 +1000

    Define component

---
+++ client/src/components/DashboardCard.jsx
@@ -0,0 +1,18 @@
+import { Flex } from "@chakra-ui/react";
+
+const DashboardCard = ({ children, ...props }) => {
+  return (
+    <Flex
+      bg="white"
+      p={4}
+      rounded="md"
+      boxShadow="md"
+      flexDirection={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      {...props}>
+      {children}
+    </Flex>
+  );
+};
+export default DashboardCard;
